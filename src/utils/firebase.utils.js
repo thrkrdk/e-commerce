a762f98ed267452a65a2e8b3386b6de56d1ac95a
thrkrdk <thrkardak@gmail.com>
2022-05-15 20:05:59 +0300

    settings

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -7,12 +7,7 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANvQobLyos4dB0QLSJP46-V2qh63DwFpg",
-  authDomain: "react-e-commerce-db-b31e0.firebaseapp.com",
-  projectId: "react-e-commerce-db-b31e0",
-  storageBucket: "react-e-commerce-db-b31e0.appspot.com",
-  messagingSenderId: "459174137600",
-  appId: "1:459174137600:web:7881ea5d3890563e47ade5",
+  
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
