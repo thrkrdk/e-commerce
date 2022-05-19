24a7666c2bac011a0854986a24335ea00ec92895
thrkrdk <thrkardak@gmail.com>
2022-05-19 16:36:31 +0300

    .

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ProductCard from "../../components/product-card/product-card.componet";
+import { CategoriesContext } from "../../contexts/categories.context";
+import "./category.styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+  console.log("category", category);
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <h2 className="category-title">{category.toLocaleUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
+  );
+};
+export default Category;
--- src/routes/shop/shop.compnent.jsx
+++ src/routes/shop/shop.compnent.jsx
@@ -1,23 +1,14 @@
-import { Fragment, useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.componet";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product}></ProductCard>
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
